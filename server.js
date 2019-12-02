10369a487eed7c0b473d8f5b24a52d45c8679f99
edyerastov <edleft4mad@gmail.com>
2019-12-02 09:14:19 +0200

    proxy config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const HttpsProxyAgent = require('https-proxy-agent');
 
 const app = express();
 
@@ -12,3 +13,43 @@ app.get('/*', function(req, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+/*
+ * API proxy configuration.
+ * This allows you to proxy HTTP request like `http.get('/api/stuff')` to another server/port.
+ * This is especially useful during app development to avoid CORS issues while running a local server.
+ * For more details and options, see https://angular.io/guide/build#using-corporate-proxy
+ */
+const proxyConfig = [
+  {
+    context: '/api',
+    pathRewrite: { '^/': '' },
+    target: 'http://10.0.0.4',
+    changeOrigin: true,
+    secure: false
+  }
+];
+
+/*
+ * Configures a corporate proxy agent for the API proxy if needed.
+ */
+function setupForCorporateProxy(proxyConfig) {
+  if (!Array.isArray(proxyConfig)) {
+    proxyConfig = [proxyConfig];
+  }
+
+  const proxyServer = process.env.http_proxy || process.env.HTTP_PROXY;
+  let agent = null;
+
+  if (proxyServer) {
+    console.log(`Using corporate proxy server: ${proxyServer}`);
+    agent = new HttpsProxyAgent(proxyServer);
+    proxyConfig.forEach(entry => {
+      entry.agent = agent;
+    });
+  }
+
+  return proxyConfig;
+}
+
+module.exports = setupForCorporateProxy(proxyConfig);
