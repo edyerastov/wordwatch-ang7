559302612b8e908027ebccd06abcca41f2a852c6
edyerastov <edleft4mad@gmail.com>
2019-12-09 10:57:15 +0200

    server

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const path = require('path');
 
 const app = express();
 
+app.use(app.router);
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/wordwatch-ang7'));
 
-app.get('/*', function(req, res) {
+app.get('/*', function(req, res, next) {
   res.sendFile(path.join(__dirname + '/dist/wordwatch-ang7/index.html'));
+  res.setHeader('Last-Modified', new Date().toUTCString());
+  next();
 });
 
 // Start the app by listening on the default Heroku port
