78fbed50a6dfd87656f920f8589f875f7a3a15ef
edyerastov <edleft4mad@gmail.com>
2019-12-09 08:10:54 +0200

    proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@
 export const environment = {
   production: true,
   version: '1.0.0',
-  serverUrl: 'http://10.0.0.4/api',
+  serverUrl: '/api',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR']
 };
