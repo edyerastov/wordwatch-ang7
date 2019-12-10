ad0a9df3aaf929c596b7e7f4baf08b99508d2e9e
edyerastov <edleft4mad@gmail.com>
2019-12-10 11:19:11 +0200

    server

--- server.js
+++ server.js
@@ -10,15 +10,13 @@ app.use(express.static(__dirname + '/dist/wordwatch-ang7'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/wordwatch-ang7/index.html'));
 });
-module.exports = function(app) {
-  app.use(
-    proxy('/api', {
-      target: 'http://10.0.0.4/',
-      changeOrigin: true,
-      pathRewrite: { '^/': '' }
-    })
-  );
-};
+app.use(
+  proxy('/api', {
+    target: 'http://10.0.0.4/',
+    changeOrigin: true,
+    pathRewrite: { '^/': '' }
+  })
+);
 app.use(cors({ origin: 'https://wordwatch-ang7.herokuapp.com/' }));
 
 // Start the app by listening on the default Heroku port
