1649436ef589ec5d35d15338fea592931e114c7d
edyerastov <edleft4mad@gmail.com>
2019-12-10 09:05:46 +0200

    server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const proxy = require('http-proxy-middleware');
+const cors = require('cors');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/wordwatch-ang7'));
-
 app.use(
   '/api',
   proxy({
@@ -15,6 +15,7 @@ app.use(
     pathRewrite: { '^/': '' }
   })
 );
+app.use(cors({ origin: 'https://wordwatch-ang7.herokuapp.com/' }));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/wordwatch-ang7/index.html'));
