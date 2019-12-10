3adf4cb6486174223dc93012969abe373bda1525
edyerastov <edleft4mad@gmail.com>
2019-12-10 11:34:22 +0200

    server

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require('path');
 const proxy = require('http-proxy-middleware');
 const cors = require('cors');
 
+const apiProxy = proxy('/api', {
+  target: 'http://10.0.0.4/',
+  changeOrigin: true,
+  pathRewrite: { '^/': '' }
+});
+
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -10,14 +16,10 @@ app.use(express.static(__dirname + '/dist/wordwatch-ang7'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/wordwatch-ang7/index.html'));
 });
-app.use(
-  proxy('/api', {
-    target: 'http://10.0.0.4/',
-    changeOrigin: true,
-    pathRewrite: { '^/': '' }
-  })
-);
+
 app.use(cors({ origin: 'https://wordwatch-ang7.herokuapp.com/' }));
 
+app.use(apiProxy);
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
