b10bb5c34aef6699945cfa5c7e9d511b3c4fbf3f
edyerastov <edleft4mad@gmail.com>
2019-12-12 00:55:57 +0200

    server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const proxy = require('http-proxy-middleware');
+const herokuProxy = require('heroku-proxy');
 
 const apiProxy = proxy('/api', {
   target: 'http://52.164.223.244/#',
@@ -16,7 +17,14 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/wordwatch-ang7/index.html'));
 });
 
-app.use(apiProxy);
+// app.use(apiProxy);
+
+app.use(
+  herokuProxy({
+    hostname: 'http://10.0.0.4',
+    port: 80
+  })
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
